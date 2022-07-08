bb7b4fce21a7c5761391103b46a04f7e6dd7d219
pm24mcgi <pmcginn@highlinefm.com>
2022-07-08 16:06:31 -0400

    cleanup

--- react-app/src/components/projects/editProjects.js
+++ react-app/src/components/projects/editProjects.js
@@ -101,7 +101,7 @@ const EditProjects = ({id, project}) => {
             <Rating onClick={handleFavorite} ratingValue={favoriteTGL} emptyColor={'rgb(211, 211, 211)'} fillColor={'rgb(255,255,0)'} size={20} initialValue={0} allowHover={false} iconsCount={1} />
           </div>
           <button className="Submit Btn" onClick={onSubmit}>
-          Edit Project???????
+          Edit Project
           </button>
         </form>
       </div>
