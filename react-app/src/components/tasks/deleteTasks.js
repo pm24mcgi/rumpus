6baab18bf097c34b72b2b066f7b6d9df7f5f58d5
pm24mcgi <pmcginn@highlinefm.com>
2022-07-11 11:37:03 -0400

    git commit -m

---
+++ react-app/src/components/tasks/deleteTasks.js
@@ -0,0 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { deleteTask, getTasks } from '../../store/tasks'
+
+const DeleteTask = ({id}) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    dispatch(deleteTask(id))
+    dispatch(getTasks())
+  };
+
+  return (
+    <button onClick={handleDelete}>
+      Delete Task
+    </button>
+  )
+}
+
+export default DeleteTask
