f1669f5593b8438856091305c9af2fed9af599e0
pm24mcgi <pmcginn@highlinefm.com>
2022-07-13 09:53:28 -0400

    splash additions

--- react-app/src/components/Splash.js
+++ react-app/src/components/Splash.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Splash1 from '../images/splashpage/splash1.png'
+import Splash2 from '../images/splashpage/splash2.png'
+import Splash3 from '../images/splashpage/splash3.png'
 
 function Splash() {
   return (
-    <h1>Splash Page</h1>
+    <>
+      <h1>Splash Page</h1>
+      <img src={Splash1}></img>
+      <img src={Splash2}></img>
+      <img src={Splash3}></img>
+    </>
   )
 }
 
