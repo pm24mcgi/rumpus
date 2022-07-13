0772274e9af4bb79154a0a161da25f34974e7f20
pm24mcgi <pmcginn@highlinefm.com>
2022-07-13 09:43:31 -0400

    navbar changes and demo user

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,28 +1,50 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import LogoutButton from './auth/LogoutButton';
 import PostTask from '../components/tasks/postTasks'
 import '../css/main.css'
 
 const NavBar = () => {
+  const sessionUser = useSelector(state => state.session.user)
+  let sessionLinks;
+
+  if (sessionUser) {
+    sessionLinks = (
+    <>
+      <nav className='NavBarContainer'>
+        <NavLink to='/' exact={true} activeClassName='active' className='NavBarElement'>
+          Home
+        </NavLink>
+        <PostTask />
+        <LogoutButton />
+      </nav>
+    </>)
+  } else {
+    sessionLinks = (
+    <>
+      <nav className='NavBarContainer'>
+        <NavLink to='/splash' exact={true} activeClassName='active' className='NavBarElement'>
+          Home
+        </NavLink>
+        <NavLink to='/login' exact={true} activeClassName='active' className='NavBarElement'>
+          Login
+        </NavLink>
+        <NavLink to='/sign-up' exact={true} activeClassName='active' className='NavBarElement'>
+          Sign Up
+        </NavLink>
+        {/* <NavLink to='/users' exact={true} activeClassName='active' className='NavBarElement'>
+          Users
+        </NavLink> */}
+      </nav>
+    </>)
+  }
+
   return (
-    <nav className='NavBarContainer'>
-      <NavLink to='/' exact={true} activeClassName='active' className='NavBarElement'>
-        Home
-      </NavLink>
-      <NavLink to='/login' exact={true} activeClassName='active' className='NavBarElement'>
-        Login
-      </NavLink>
-      <NavLink to='/sign-up' exact={true} activeClassName='active' className='NavBarElement'>
-        Sign Up
-      </NavLink>
-      <NavLink to='/users' exact={true} activeClassName='active' className='NavBarElement'>
-        Users
-      </NavLink>
-      {/* <PostTask /> */}
-      <LogoutButton />
-    </nav>
+    <div>
+      {sessionLinks}
+    </div>
   );
 }
 
